2d670098e5f8db9bd8116491b7871397af9cc178
Arvand Alviri <aalviri@gmail.com>
2015-03-02 14:33:08 -0800

    decline issue fixed

--- app/controllers/declines_controller.rb
+++ app/controllers/declines_controller.rb
@@ -1,11 +1,25 @@
 class DeclinesController < ApplicationController
 
-	def realdecline
-  	@trip = Trip.find(params[:id]) #anything your finding something, when u need access to a specific trip / entry, u pass ID u want to manipulate
-  	@trip.declined_at = Time.now
-  	@trip.user_id = current_user.id
-  	@trip.save
-  	redirect_to :back
+  def realdecline
+	@decline = Decline.new#(decline_params)  #creating a brand new entry
+	@current_trip = Trip.find(params[:id])
+
+	@decline.user_id = current_user.id
+	@decline.trip_id = @current_trip.id
+	@decline.save
+	redirect_to :back, :notice => "You declined #{@current_trip.email}."
+
+
+  	#@decline = Trip.find(params[:id]) #anything your finding something, when u need access to a specific trip / entry, u pass ID u want to manipulate
+  	#@decline.user_id = current_user.id
+  	#@decline.trip_id = @decline
+  	#@decline.save
+  	#redirect_to :back
   end
+
+  private
+	def decline_params
+    	params.require(:decline).permit(:user_id)
+  	end
 
 end
--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -6,11 +6,19 @@ class StaticController < ApplicationController
     @charge_guid_entered = Trip.new  #DONT NEED THIS. added this because other wise it was saying nil value for charge_guid_entered
     
 
+
+
+
+
     # @unresponded = Trip.unresponded #no one has accepted and no one has declined these people.
     @paid = Trip.paid #need to change is its host (user) specific
   	if user_signed_in?
 	  	@my_trips = Trip.where("user_id = ?", current_user.id) #find trips where user id equals current user id
       @unresponded = Trip.unresponded #no one has accepted and no one has declined these people.
+      @currentuser_declined_this = Decline.where("user_id = ?", current_user.id).pluck(:trip_id) #grab the trip id's from the declines table if the user id is the current user
+      
+      #render json: @currentuser_declined_this and return
+      @user_can_accept_these = Trip.unaccepted.where.not(id: @currentuser_declined_this) #where the id is not this function. 
 	   end
 
      @peopletocharge = Trip.peopletocharge
